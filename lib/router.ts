d44fa0a5f99aea82d1b655efcfcbfe5a0dbf528f
Amely Kling <mail@dwi.sk>
2025-03-30 18:39:32 +0200

    test buildjs

--- lib/router.ts
+++ lib/router.ts
@@ -19,12 +19,12 @@ export default class MinitelTSRouter {
     console.log(appDir)
     const files = fs.readdirSync(appDir);
     for (const file of files) {
-      if (file.endsWith('.ts')) {
+      if (file.endsWith('.ts') || file.endsWith('.js')) {
         const p = path.join(appDir, file);
         const a = await import(p);
         this.routes.push({
-          path: file.replace('.ts', ''),
-          name: a.name || file.replace('.ts', ''),
+          path: file.replace('.ts', '').replace('.js', ''),
+          name: a.name || file.replace('.ts', '').replace('.js', ''),
           loop: a.default,
           state: MinitelTSState({initial:a.initialState})
         });
@@ -32,7 +32,14 @@ export default class MinitelTSRouter {
 
       // if file is directory, check for directory/index.ts and add it to routes
       if (fs.statSync(path.join(appDir, file)).isDirectory()) {
-        const indexFile = path.join(appDir, file, 'index.ts');
+        const indexFileJS = path.join(appDir, file, 'index.js');
+        const indexFileTS = path.join(appDir, file, 'index.ts');
+        let indexFile = '';
+        if (fs.existsSync(indexFileJS)) {
+          indexFile = indexFileJS;
+        } else if (fs.existsSync(indexFileTS)) {
+          indexFile = indexFileTS;
+        } 
         if (fs.existsSync(indexFile)) {
           const a = await import(indexFile);
           this.routes.push({
